f4964af4dcbbb561d85f78fbebef45a1d70210ac
Mike Fang <mike1026915.cs96@g2.nctu.edu.tw>
2023-09-23 18:17:29 +0800

    update

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(logger('dev'));
 app.use(cors())
-app.use(helmet());
+//app.use(helmet());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
